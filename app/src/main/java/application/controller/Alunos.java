5a866dd50834a98ab4e6d4943207e535971975fe
luis-cavano <luissevilhadev@gmail.com>
2024-08-19 17:42:20 +0000

    [CHD] conteudo da aula de 19 de agosto de 2024

--- app/src/main/java/application/controller/Alunos.java
+++ app/src/main/java/application/controller/Alunos.java
@@ -1,14 +1,23 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.model.Aluno;
 import application.repository.AlunoRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -23,8 +32,28 @@ public class Alunos {
         return alunoRepo.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Aluno details(@PathVariable long id){
+        return alunoRepo.findById(id).get();
+    }
+    
     @PostMapping
     public Aluno insert(@RequestBody Aluno aluno) {
         return alunoRepo.save(aluno);
+    }
+
+    @PutMapping("path/{id}")
+    public Aluno put( 
+        @PathVariable long id,    
+        @RequestBody Aluno newData
+    ){
+        Optional<Aluno> result = alunoRepo.findById(id);
+        result.get().setNome(newData.getNome());
+        return alunoRepo.save(result.get());     
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@RequestParam long id) {
+        alunoRepo.deleteById(id);
     }
 }
